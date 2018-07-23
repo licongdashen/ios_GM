fca2582b4208d9d438b3e72d4d46765935fbfe95
Apple <827661334@qq.com>
2018-07-23 16:36:55 +0800

    no message

--- ios_GM/ios_GM/SuspendBeginViewController.m
+++ ios_GM/ios_GM/SuspendBeginViewController.m
@@ -9,8 +9,12 @@
 #import "SuspendBeginViewController.h"
 #import <CoreMotion/CoreMotion.h>
 #import "SuspendSucViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface SuspendBeginViewController ()
+@interface SuspendBeginViewController ()<CLLocationManagerDelegate>
+{
+    CLLocationManager *_manger;
+}
 
 @property (nonatomic) CMMotionManager *motionManager;
 
@@ -29,12 +33,14 @@
 -(void)viewWillDisappear:(BOOL)animated
 {
     [self.motionManager stopAccelerometerUpdates];
+    [_manger stopUpdatingLocation];
+
 }
 
 -(void)kaishi
 {
     [self.motionManager stopAccelerometerUpdates];
-
+    [_manger stopUpdatingLocation];
     SuspendSucViewController *vc = [[SuspendSucViewController alloc]init];
     [self.navigationController pushViewController:vc animated:YES];
 }
@@ -87,22 +93,119 @@
     [self addObserver: self forKeyPath: @"data" options: NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context: nil];
 
 
+    _manger = [[CLLocationManager alloc] init];
+    
+    //设置位置管理者对象的代理
+    _manger.delegate = self;
+    
+    //设置定位精度（精度越高越耗电，定位时间越长）
+    _manger.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    //设置定位间隔（每移动多远获取一次定位信息）
+    // _manger.distanceFilter = 10;
+    
+    //设置定位授权
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 9.0)
+    {
+        _manger.allowsBackgroundLocationUpdates = YES;
+    }
+    [_manger requestWhenInUseAuthorization];
+    [_manger startUpdatingLocation];
+    [_manger location];
+    
+    [self countDistance];
+}
+
+//当定位权限的授权状态发生改变时调用该方法
+-(void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    switch (status)
+    {
+            //1、尚未确定是否授权
+        case kCLAuthorizationStatusNotDetermined:
+            
+            NSLog(@"用户尚未确定是否授权！");
+            [CACUtility showTips:@"用户尚未确定是否授权！"];
+
+            break;
+            
+            //2、访问权限受限
+        case kCLAuthorizationStatusRestricted:
+            
+            NSLog(@"获取定位权限授权状态受限！");
+            [CACUtility showTips:@"获取定位权限授权状态受限！"];
+
+            break;
+            
+            //3、获取定位权限被拒绝
+        case kCLAuthorizationStatusDenied:
+            
+            // 定位服务是否可用（用户是否开启了手机上的定位开关）
+            if([CLLocationManager locationServicesEnabled])
+            {
+                NSLog(@"手机定位功能开启，但被拒绝");
+                [CACUtility showTips:@"手机定位功能开启，但被拒绝"];
+
+            }
+            
+            else
+            {
+                NSLog(@"手机定位功能关闭，不可用");
+                [CACUtility showTips:@"手机定位功能关闭，不可用"];
+
+            }
+            
+            break;
+            
+            //4、获取前台定位权限
+        case kCLAuthorizationStatusAuthorizedWhenInUse:
+            
+            NSLog(@"获取了前台定位权限！");
+//            [CACUtility showTips:@"获取了前台定位权限！"];
+
+            break;
+            
+            //5、获取后台定位权限
+        case kCLAuthorizationStatusAuthorizedAlways:
+            
+            NSLog(@"获取了后台定位权限！");
+//            [CACUtility showTips:@"获取了后台定位权限！"];
+
+            break;
+            
+    }
+}
+
+-(void)countDistance
+{
+    CLLocation *loc1=[[CLLocation alloc]initWithLatitude:40 longitude:116];
+    CLLocation *loc2=[[CLLocation alloc]initWithLatitude:41 longitude:116];
+    CLLocationDistance distance=[loc1 distanceFromLocation:loc2];
+     NSLog(@"(%@)和(%@)的距离=%fM",loc1,loc2,distance);
+}
+
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    [CACUtility showTips:@"获取位置信息失败！"];
+
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations
+{
+    CLLocation *currentLocation = (CLLocation *)[locations lastObject];
+    NSLog(@"纬度=%f，经度=%f",currentLocation.coordinate.latitude,currentLocation.coordinate.longitude);
+    
 }
 
 
 -(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
     if ([keyPath isEqualToString:@"data"]) {
-//        id oldName = [change objectForKey:NSKeyValueChangeOldKey];
-//        if (![DEF_OBJECT_TO_STIRNG(oldName) isEqualToString:@""]) {
-//            CMAccelerometerData * _Nullable oldaccelerometerData = oldName;
-//            NSLog(@"oldName----------%@",[NSString stringWithFormat:@"加速度为\n---------\nX轴：%+.2f\nY轴：%+.2f\nZ轴：%+.2f",oldaccelerometerData.acceleration.x,oldaccelerometerData.acceleration.y,oldaccelerometerData.acceleration.z]);
-//        }
-        
+
         id newName = [change objectForKey:NSKeyValueChangeNewKey];
         if (![DEF_OBJECT_TO_STIRNG(newName) isEqualToString:@""]) {
             CMAccelerometerData * _Nullable newaccelerometerData = newName;
-            NSLog(@"newName-----------%@",[NSString stringWithFormat:@"加速度为\n---------\nX轴：%+.2f\nY轴：%+.2f\nZ轴：%+.2f",newaccelerometerData.acceleration.x,newaccelerometerData.acceleration.y,newaccelerometerData.acceleration.z]);
+//            NSLog(@"newName-----------%@",[NSString stringWithFormat:@"加速度为\n---------\nX轴：%+.2f\nY轴：%+.2f\nZ轴：%+.2f",newaccelerometerData.acceleration.x,newaccelerometerData.acceleration.y,newaccelerometerData.acceleration.z]);
             
             double x = fabs(newaccelerometerData.acceleration.x);
             double y = fabs(newaccelerometerData.acceleration.y);
